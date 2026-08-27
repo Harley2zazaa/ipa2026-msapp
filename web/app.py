e21f63d23b40248b4366fc4879a4be21fa175228
Harley2zazaa <67070259@kmitl.ac.th>
2026-08-27 20:49:28 +0700

    67

--- web/app.py
+++ web/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import redirect
+from flask import url_for
 
 app = Flask(__name__)
 
@@ -13,13 +14,26 @@ def main():
 
 @app.route("/add", methods=["POST"])
 def add_comment():
-    yourname = request.form.get("yourname")
-    message = request.form.get("message")
-
-    if yourname and message:
-        data.append({"yourname": yourname, "message": message})
+    # yourname = request.form.get("yourname")
+    # message = request.form.get("message")
+    ip = request.form.get("ip")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    
+    if ip and username and password:
+        data.append({"ip": ip, "username": username, "password":password})
     return redirect("/")
 
+
+@app.route("/delete", methods=["POST"])
+def delete_comment():
+    try:
+        idx = int(request.form.get("idx"))
+        if 0 <= idx < len(data):
+            data.pop(idx)
+    except Exception:
+        pass
+    return redirect(url_for("main"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
