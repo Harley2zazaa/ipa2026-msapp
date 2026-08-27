3f5550c809f42a4deb2601b848a81549b629293f
Harley2zazaa <67070259@kmitl.ac.th>
2026-08-27 21:52:27 +0700

    9

--- app.py
+++ app.py
@@ -20,6 +20,5 @@ def add_comment():
         data.append({"yourname": yourname, "message": message})
     return redirect("/")
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
