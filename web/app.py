32ea7a3b52b9aa31f3ada910b41b397bf6bf5056
Harley2zazaa <67070259@kmitl.ac.th>
2026-08-27 21:45:59 +0700

    8

--- web/app.py
+++ web/app.py
@@ -3,37 +3,45 @@ from flask import request
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
+client = MongoClient("mongodb://mongo:27017/")
+mydb = client["ipa2026"]
+mycol = mydb["router"]
 
 data = []
 
 @app.route("/")
 def main():
+    data = list(mycol.find()) 
     return render_template("index.html", data=data)
 
 @app.route("/add", methods=["POST"])
 def add_comment():
-    # yourname = request.form.get("yourname")
-    # message = request.form.get("message")
     ip = request.form.get("ip")
     username = request.form.get("username")
     password = request.form.get("password")
     
     if ip and username and password:
-        data.append({"ip": ip, "username": username, "password":password})
+        mycol.insert_one({
+            "ip": ip,
+            "username": username,
+            "password" : password,
+        })
     return redirect("/")
 
 
 @app.route("/delete", methods=["POST"])
 def delete_comment():
-    try:
-        idx = int(request.form.get("idx"))
-        if 0 <= idx < len(data):
-            data.pop(idx)
-    except Exception:
-        pass
-    return redirect(url_for("main"))
+    ip = request.form.get("ip")
+    
+    if ip:
+        # ลบ Document ที่มี ip ตรงกัน ออกจาก MongoDB
+        mycol.delete_one({"ip": ip})
+        
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
