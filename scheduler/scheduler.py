120f9f3509b7b427eafbb0bbc36f5ac12e7c40f0
Harley2zazaa <67070259@kmitl.ac.th>
2026-08-28 19:18:08 +0700

    modify scheduler

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -6,12 +6,15 @@ from producer import produce
 def scheduler():
     count = 0
     while True:
+
         now = time.time()
         now_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
-        print(f"[{now_str}] run #{count}")
+        ms = int((now % 1) * 1000)
+        now_str_with_ms = f"{now_str}.{ms:03d}"
+        print(f"[{now_str_with_ms}] run #{count}")
 
         try:
-            produce("localhost", "192.168.1.44")
+            produce("localhost", "192.168.1.1")
         except Exception as e:
             print(e)
             time.sleep(3)
