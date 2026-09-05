94892222156735c7c42b83e50da7c2a09214c32d
Harley2zazaa <67070259@kmitl.ac.th>
2026-09-05 19:55:49 +0700

    modify_step4.2

--- worker/router_clinet.py
+++ worker/router_clinet.py
@@ -1,23 +1,21 @@
 from netmiko import ConnectHandler
 import ntc_templates, os, json
 
-def get_interfaces():
+def get_interfaces(ip, username, password):
 
     os.environ["NET_TEXTFSM"] = os.path.join(
-    os.path.dirname(ntc_templates.__file__), "templates"
+        os.path.dirname(ntc_templates.__file__), "templates"
     )
 
     device = {
         "device_type": "cisco_ios",
-        "host": "10.0.29.101",
-        "username": "admin",
-        "password": "cisco",
-        "secret": "cisco",
+        "host": ip,
+        "username": username,
+        "password": password,
     }
 
     with ConnectHandler(**device) as conn:
-        if not conn.check_enable_mode():
-            conn.enable()
+        # conn.enable()
         result = conn.send_command("show ip int br", use_textfsm=True)
         conn.disconnect()
 
